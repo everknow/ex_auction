b6c096bf483b2a6b6103876fa0de15d93dbb3359
Bruno Ripa <bruno.ripa@gmail.com>
2021-05-19 15:42:41 +0100

    Added bid_value to websocket notification

--- apps/ex_auctions_manager/lib/ex_auctions_manager/endpoints/bid/v1/receiver.ex
+++ apps/ex_auctions_manager/lib/ex_auctions_manager/endpoints/bid/v1/receiver.ex
@@ -45,7 +45,7 @@ defmodule ExAuctionsManager.Bids.V1.Receiver do
 
     case DB.create_bid(auction_id, bid_value, bidder) do
       {:ok, %Bid{auction_id: ^auction_id, bid_value: ^bid_value, bidder: ^bidder}} ->
-        WebsocketUtils.notify_bid(auction_id)
+        WebsocketUtils.notify_bid(auction_id, bid_value)
 
         json_resp(conn, 201, %{auction_id: auction_id, bid_value: bid_value, bidder: bidder})
 
