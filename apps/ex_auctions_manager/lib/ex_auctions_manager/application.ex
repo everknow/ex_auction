79fecf49886f79c1849e513b654ac83f01ab61bc
Bruno Ripa <bruno.ripa@gmail.com>
2021-06-07 21:06:36 +0100

    Work in progress

--- apps/ex_auctions_manager/lib/ex_auctions_manager/application.ex
+++ apps/ex_auctions_manager/lib/ex_auctions_manager/application.ex
@@ -2,9 +2,12 @@ defmodule ExAuctionsManager.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
+    Logger.info("Redis host: #{Application.get_env(:ex_contract_cache, :redis_host)}")
+
     children = [
       Plug.Cowboy.child_spec(
         # This must come from config, so that it can be https on prod
